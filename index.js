4c63cef94ffa3248a5533705c0d95ddeac4b56ee
sam ghoreishi <sam20gh@gmail.com>
2018-12-30 16:33:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@
   
 function theBeatlesPlay(musicians, instruments){
   var play =[];
-  for(var  i=0;i < theBeatlesPlay.length; i++){
+  for(var  i= 0 ; i < musicians.length; i++){
     play.push(`${musicians[i]} plays ${instruments[i]}`)
     }
     return play
