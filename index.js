ccf6c2138be709f7889ffe195b738d351d8ade2e
sam ghoreishi <sam20gh@gmail.com>
2018-12-30 16:31:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
- function theBeatlesPlay(musicians, instruments) {
-  var play = [];
-   for(var i = 0; i < musicians.length; i++) {
-    play.push(`${musicians[i]} plays ${instruments[i]}`);
-  }
-  return play;
- }
-function johnLennonFacts(facts) {
-  var array = [];
-  let i = 0;
-  while (i < facts.length) {
-    array.push(`${facts[i]}!!!`);
-    i++;
-  }
-  return array
-}
+// function theBeatlesPlay(musicians, instruments) {
+//   var play = [];
+//   for(var i = 0; i < musicians.length; i++) {
+//     play.push(`${musicians[i]} plays ${instruments[i]}`);
+//   }
+//   return play;
+// }
+// function johnLennonFacts(facts) {
+//   var array = [];
+//   let i = 0;
+//   while (i < facts.length) {
+//     array.push(`${facts[i]}!!!`);
+//     i++;
+//   }
+//   return array
+// }
 
 
-function iLoveTheBeatles(number){
-  var love=[];
-  var i=0;
-  do {
-    love.push("I love the Beatles!");
-    i++;
-  }
-   while ( i <= number && number < 15);
-  return love;
-}
+// function iLoveTheBeatles(number){
+//   var love=[];
+//   var i=0;
+//   do {
+//     love.push("I love the Beatles!");
+//     i++;
+//   }
+//   while ( i <= number && number < 15);
+//   return love;
+// }
   
-
+function theBeatlesPlay(musicians, instruments){
+  var play =[];
+  for(var  i=0;i < theBeatlesPlay.length; i++){
+    play.push(`${musicians[i]} plays $`)
+    
+  }
+}
 
